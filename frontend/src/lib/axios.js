12ab8a95af0507dc431393e8301397787bd07676
Dues-ExMachina <anubhabd45@gmail.com>
2025-07-19 00:52:34 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+    baseURL: import.meta.env.MODE === "development" 
+        ? "http://localhost:5001/api" 
+        : "https://hikechatapp-production.up.railway.app/api",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",

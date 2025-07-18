70287bdbb241e417cd8ee2b0012b7996ecb625f6
Dues-ExMachina <anubhabd45@gmail.com>
2025-07-19 01:07:36 +0530

    Update useAuthStore.js  BASE_URL

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,8 +4,12 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
+//For localdevelopment
+//const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/"; 
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+const BASE_URL = import.meta.env.MODE === "development" 
+    ? "http://localhost:5001" 
+    : "https://hikechatapp-production.up.railway.app";
 
 export const useAuthStore = create((set, get) => ({
     //initially we dont know if user is authenticated so null
